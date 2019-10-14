ffe3343794dbe78114d041b999b1d7c0b0ae2b48
kronborgkvalvik <kronborgkvalvik@gmail.com>
2019-10-14 10:40:35 +0200

    endring av henriette

---
+++ Admin.js
@@ -0,0 +1,8 @@
+class Admin {
+constructor(username, password) {
+    this.username = username;
+    this.password = password;
+}
+}
+
+var admin1 = new Admin(admin, 12345);
---
+++ UserDB.js
@@ -0,0 +1,10 @@
+class Client {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+Client{
+    var client1= new Client(Markus, 1998);
+}
