00213bf5c33ef16f241c1f9a820d98f5c2af22ee
Mikkel0608 <mikkel_marcher@hotmail.com>
2019-10-15 10:46:13 +0200

    mikkel

--- Javascript/orderPage.js
+++ Javascript/orderPage.js
@@ -1,6 +1,5 @@
-
 function confirmTime() {
-    // creating variables that represents the user selection//
+    // creating variables that represents the user selection of date and time
     var rentDayID = document.getElementById("rentDay");
     var rentDayValue = rentDayID.options[rentDayID.selectedIndex].value;
     var rentMonthID = document.getElementById("rentMonth");
@@ -11,11 +10,31 @@ function confirmTime() {
     var rentTimeID = document.getElementById("rentTime");
     var rentTimeValue = rentTimeID.options[rentTimeID.selectedIndex].value;
 
-//Tests if the var set before are equal to 00 (haven't been set). If they are not, it shows the jetskis)
+//Tests if the vars set before are equal to 00 (haven't been set). If they are not, it shows the jetskis)
     if (rentDayValue != "00" && rentMonthValue != "00" && rentYearValue != "00" && rentTimeValue != "00") {
-        document.getElementById("modelContainer1").style.display = ""
-        document.getElementById("modelContainer2").style.display = ""
-        document.getElementById("modelContainer3").style.display = ""
+        document.getElementById("modelContainer1").style.display = "";
+        document.getElementById("modelContainer2").style.display = "";
+        document.getElementById("modelContainer3").style.display = "";
+    } else {
+        alert("Udfyld venligst alle felter.")
     }
 
+}
+
+class Jetski {
+    constructor(model, price, HorsePower) {
+        this.model = model;
+        this.price = price;
+        this.HorsePower = HorsePower;
+    }
+}
+
+var jetski1= new Jetski('Sea Doo Spark', 3, 60);
+var jetski2= new Jetski('Yamaha Waverunner VX', 5, 125);
+var jetski3= new Jetski('Kawasaki STX 15F', 7, 160);
+
+
+function calculatePrice() {
+    var orderAmountJS = document.getElementById('orderAmount').value;
+    document.getElementById('orderPrice').innerHTML = orderAmountJS*3;
 }
