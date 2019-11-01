f4abde89717f90cf039d0b6678acdfa82f1f9b40
Mikkel0608 <mikkel_marcher@hotmail.com>
2019-11-01 09:11:57 +0100

    mikkel

--- Javascript/orderConfirmation.js
+++ Javascript/orderConfirmation.js
@@ -2,15 +2,15 @@ window.onload = function getOrderInfo() {
     //Inserts the user ID in the navibar
     document.getElementById('loginPhone').innerHTML="Logget ind med ID: <br>" + localStorage.getItem('phone');
     //Inserts the order information on the order confirmation
-    var day = localStorage.getItem('storedDay');
-    var month = localStorage.getItem('storedMonth');
-    var year = localStorage.getItem('storedYear');
+    var day = localStorage.getItem('orderDay');
+    var month = localStorage.getItem('orderMonth');
+    var year = localStorage.getItem('orderYear');
     document.getElementById('date').innerHTML ="Dato for udlejning: "+ day + "/" + month+"/"+year;
-    document.getElementById('timePeriod').innerHTML ="Tidspunkt for udlejning: kl. " + localStorage.getItem('storedTimePeriod');
-    document.getElementById('amountOfJetski1').innerHTML ="Antal Sea Doo Spark: " + localStorage.getItem('storedJetski1');
-    document.getElementById('amountOfJetski2').innerHTML ="Antal Yamaha Waverunner VX: " + localStorage.getItem('storedJetski2');
-    document.getElementById('amountOfJetski3').innerHTML ="Antal Kawasaki STX-15F: " + localStorage.getItem('storedJetski3');
-    document.getElementById('orderPrice').innerHTML = "Samlet pris til betaling ved udlejning: " + localStorage.getItem('storedOrderPrice');
+    document.getElementById('timePeriod').innerHTML ="Tidspunkt for udlejning: kl. " + localStorage.getItem('timePeriod');
+    document.getElementById('amountOfJetski1').innerHTML ="Antal Sea Doo Spark: " + localStorage.getItem('amountOfJetski1');
+    document.getElementById('amountOfJetski2').innerHTML ="Antal Yamaha Waverunner VX: " + localStorage.getItem('amountOfJetski2');
+    document.getElementById('amountOfJetski3').innerHTML ="Antal Kawasaki STX-15F: " + localStorage.getItem('amountOfJetski3');
+    document.getElementById('orderPrice').innerHTML = "Samlet pris til betaling ved udlejning: " + localStorage.getItem('orderPrice');
 }
 
 function checkLoginOrderPage() {
