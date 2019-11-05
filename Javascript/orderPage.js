cec992f6839f0abfafe0b2b051d46bb64ecc6bb1
MortenDyberg <dyberg@live.dk>
2019-11-05 09:33:09 +0100

    .

--- Javascript/orderPage.js
+++ Javascript/orderPage.js
@@ -159,40 +159,6 @@ class Order {
 
     orderArray.push(order1, order2, order3, order4, order5, order6);
 
- /* denne kode virker ikke   storeOrder(){
-        localStorage.setItem('amount1', amount1.value);
-        localStorage.setItem('amount2', amount2.value);
-        localStorage.setItem('amount3', amount3.value);
-        localStorage.setItem('orderDay', orderDay.value);
-        localStorage.setItem('orderMonth', orderMonth.value);
-        localStorage.setItem('orderYear', orderYear.value);
-        localStorage.setItem('timePeriod', timePeriod.value);
-        localStorage.setItem('orderPrice', finalPrice.value);
-
-    }
-
-}
-
-function registerOrder(){
-    var amount1 = document.getElementById('orderAmount1').value;
-    var amount2 = document.getElementById('orderAmount2').value;
-    var amount3 = document.getElementById('orderAmount3').value;
-    var orderDay = document.getElementById('rentDay').value;
-    var orderMonth = document.getElementById('rentMonth').value;
-    var orderYear = document.getElementById('rentYear').value;
-    var timePeriod = document.getElementById('rentTime').value;
-    var orderAmount1JS = document.getElementById('orderAmount1').value;
-    var orderAmount2JS = document.getElementById('orderAmount2').value;
-    var orderAmount3JS = document.getElementById('orderAmount3').value;
-    var finalPrice = orderAmount1JS * jetski1.price + orderAmount2JS * jetski2.price + orderAmount3JS * jetski3.price;
-
-    var order1 = new order(amount1, amount2, amount3, orderDay, orderMonth, orderYear, timePeriod, finalPrice);
-
-    order1.storeOrder();
-    window.location = 'orderConfirmation.html';
-
-}
-*/
 
 
 
