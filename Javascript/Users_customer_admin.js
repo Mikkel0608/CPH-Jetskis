827bb3ee73dac6603f6316193a44f36b1c407d4f
MortenDyberg <dyberg@live.dk>
2019-12-02 14:53:48 +0100

    rettet stavefejl

--- Javascript/Users_customer_admin.js
+++ Javascript/Users_customer_admin.js
@@ -96,6 +96,13 @@ function register() {
         validation_message += "Venligst udfyld telefonnummer!";
         form_valid = false;
     }
+    var userArray = JSON.parse(localStorage.getItem("userArray"));
+    for (var i=0; i < userArray.length; i++) {
+        if (phone == userArray[i].phone) {
+            validation_message += "En bruger med det telefonnummer eksisterer allerede!";
+            form_valid = false;
+        }
+    }
 
     //6. Validating the e-mail
     if (email==null || email=="")
@@ -249,7 +256,7 @@ class Admin {
         this.password = password;
     }
     logIn(){
-        console.log(this.username, "Har lige logget ind");
+        console.log(this.username, "har lige logget ind");
     }
 }
 //We make an instance of this class by creating an object.
